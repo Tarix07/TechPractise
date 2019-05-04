971bde2723e60ef94791edf53b25778c7c381dfc
Tarix07 <taras.vons.99@gmail.com>
2019-05-04 22:15:34 +0000

    Game: methods for playing

--- Spor/rps/models.py
+++ Spor/rps/models.py
@@ -1,7 +1,7 @@
 from builtins import staticmethod
 from django.contrib.auth.models import User
 from django.db import models
-
+from datetime import datetime
 
 class Game(models.Model):
     game_name = models.TextField(max_length=50, unique=True, blank=True)
@@ -37,5 +37,46 @@ class Game(models.Model):
 
     def __str__(self):
         return self.game_name
+
+    @staticmethod
+    def get_game(name):
+        game = Game.objects.filter(game_name__exact=name)
+        if game.exists():
+            game = game.first()
+        return game
+
+    def mark_complete(self, winner):
+        self.winner = winner
+        self.completed = datetime.now()
+        self.set_status("completed")
+        self.save()
+
+    def result(self):
+        if self.creator_choice == 'rock' and self.opponent_choice == 'scissors':
+            self.mark_complete(self.creator)
+        elif self.creator_choice == 'scissors' and self.opponent_choice == 'paper':
+            self.mark_complete(self.creator)
+        elif self.creator_choice == 'paper' and self.opponent_choice == 'rock':
+            self.mark_complete(self.creator)
+        elif self.opponent_choice == 'rock' and self.creator_choice == 'scissors':
+            self.mark_complete(self.opponent)
+        elif self.opponent_choice == 'scissors' and self.creator_choice == 'paper':
+            self.mark_complete(self.opponent)
+        elif self.opponent_choice == 'paper' and self.creator_choice == 'rock':
+            self.mark_complete(self.opponent)
+        elif self.opponent_choice == self.creator_choice:
+            self.opponent_choice = ""
+            self.creator_choice = ""
+            self.save(update_fields=["opponent_choice", "creator_choice"])
+            return "tai"
+
+    def make_creator_choice(self, message):
+        self.creator_choice = message
+        self.save(update_fields=["creator_choice"])
+
+    def make_opponent_choice(self, message):
+        self.opponent_choice = message
+        self.save(update_fields=["opponent_choice"])
+
 
 
