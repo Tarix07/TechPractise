3fc79e7dd530a2423738a62a719fdab6a0ecffa8
Onufran Eugene <onufranj@gmail.com>
2019-05-24 19:19:00 +0000

    rps/views

--- Spor/rps/views.py
+++ Spor/rps/views.py
@@ -1,3 +1,34 @@
+# chat/views.py
 from django.shortcuts import render
+from django.utils.safestring import mark_safe
+import json
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+from django.contrib.auth.decorators import login_required
+from rps.models import Profile, Game
 
-# Create your views here.
+
+@login_required(login_url='/login/')
+def room(request, room_name):
+    return render(request, 'rps/room.html', {
+        'room_name_json': mark_safe(json.dumps(room_name))
+    })
+@login_required(login_url='/login/')
+def mygames (request):
+    return render(request, 'rps/mygames.html',{
+        'games_for_player': Game.get_games_for_player(request.user)[:20],
+        'profile': Profile.objects.filter(user=request.user).first()
+    })
+
+
+
+@login_required(login_url='/login/')
+def statistic(request):
+    return render(request,'rps/statistic.html',{
+       'best_player': Profile.objects.all().order_by("-rating").first(),
+        'worst_player': Profile.objects.all().order_by("rating").first(),
+        'available_games': Game.objects.filter(status="waiting").count(),
+        'completed_games': Game.objects.filter(status="completed").count(),
+        'completed_games_list': Game.objects.filter(status="completed"),
+    })
