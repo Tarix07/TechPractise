650dc4035353e044d38ad67b56f8bdef1104b109
Tarix07 <taras.vons.99@gmail.com>
2019-06-02 17:20:02 +0000

    Last fix Models

--- Spor/rps/models.py
+++ Spor/rps/models.py
@@ -1,26 +1,46 @@
-from builtins import staticmethod
 from django.contrib.auth.models import User
 from django.db import models
+import json
 from datetime import datetime
+
 
 class Game(models.Model):
     game_name = models.TextField(max_length=50, unique=True, blank=True)
     winner = models.ForeignKey(
-        User, related_name='winner', null=True, blank=True, on_delete=models.CASCADE)
-    creator = models.ForeignKey(User, related_name='creator', on_delete=models.CASCADE)
+        User, related_name='winner', null=True, blank=True, on_delete=models.DO_NOTHING)
+    creator = models.ForeignKey(
+        User, null=True,blank=True, related_name='creator', on_delete=models.DO_NOTHING)
     opponent = models.ForeignKey(
-        User, related_name='opponent', null=True, blank=True, on_delete=models.CASCADE)
+        User, related_name='opponent', null=True, blank=True, on_delete=models.DO_NOTHING)
     completed = models.DateTimeField(null=True, blank=True)
     creator_choice = models.TextField(null=True, blank=True,)
     opponent_choice = models.TextField(null=True, blank=True)
     status = models.TextField(blank=True, default="waiting")
 
+    @staticmethod
+    def get_available_games():
+        return Game.objects.filter(status__exact="waiting").order_by('game_name')[:20]
+
+    @staticmethod
+    def get_games_for_player(user):
+        from django.db.models import Q
+        return Game.objects.filter(Q(opponent=user) | Q(creator=user) & Q(status="completed"))
+
+    @staticmethod
+    def get_completed_games():
+        return Game.objects.filter(status="completed").order_by('completed')
 
     @staticmethod
     def create_new(name, user):
         new_game = Game(creator=user, game_name=name)
         new_game.save()
         return new_game
+
+    def mark_complete(self, winner):
+        self.winner = winner
+        self.completed = datetime.now()
+        self.set_status("completed")
+        self.save()
 
     def set_creator(self, user):
         self.creator = User.objects.filter(username=user)[0]
@@ -35,8 +55,6 @@ class Game(models.Model):
         self.status = status
         self.save(update_fields=["status"])
 
-    def __str__(self):
-        return self.game_name
 
     @staticmethod
     def get_game(name):
@@ -45,11 +63,8 @@ class Game(models.Model):
             game = game.first()
         return game
 
-    def mark_complete(self, winner):
-        self.winner = winner
-        self.completed = datetime.now()
-        self.set_status("completed")
-        self.save()
+    def __str__(self):
+        return self.game_name
 
     def result(self):
         if self.creator_choice == 'rock' and self.opponent_choice == 'scissors':
@@ -70,6 +85,7 @@ class Game(models.Model):
             self.save(update_fields=["opponent_choice", "creator_choice"])
             return "tai"
 
+
     def make_creator_choice(self, message):
         self.creator_choice = message
         self.save(update_fields=["creator_choice"])
@@ -77,19 +93,6 @@ class Game(models.Model):
     def make_opponent_choice(self, message):
         self.opponent_choice = message
         self.save(update_fields=["opponent_choice"])
-
-    @staticmethod
-    def get_available_games():
-        return Game.objects.filter(status__exact="waiting").order_by('game_name')[:20]
-
-    @staticmethod
-    def get_games_for_player(user):
-        from django.db.models import Q
-        return Game.objects.filter(Q(opponent=user) | Q(creator=user))
-
-    @staticmethod
-    def get_completed_games():
-        return Game.objects.filter(status="completed").order_by('completed')
 
     @staticmethod
     def get_random():
@@ -102,6 +105,7 @@ class Profile(models.Model):
     wins = models.IntegerField(blank=True, default=0)
     loses = models.IntegerField(blank=True, default=0)
     rating = models.FloatField(blank=True, default=0)
+
 
     @staticmethod
     def get_profile(user):
@@ -123,23 +127,25 @@ class Profile(models.Model):
         self.save(update_fields=['wins'])
         self.wl()
 
+
     def profile_loses(self):
         self.loses += 1
         self.save(update_fields=['loses'])
         self.wl()
 
+
     def wl(self):
         if self.loses == 0:
             winlose = self.wins
         else:
-            winlose = self.wins / self.loses
+            winlose = self.wins/self.loses
         self.rating = winlose
         self.save(update_fields=['rating'])
 
     @staticmethod
     def change_profile(user, kod):
         profile = Profile.get_profile(user)
-        if kod == 1:
+        if kod==1:
             profile.profile_wins()
         else:
             profile.profile_loses()
